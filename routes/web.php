4709619bbb8c6ef3a77874955adb9fc998e41dad
albertopiffer <albertopiffer96@gmail.com>
2023-04-11 17:48:23 +0200

    FEAT controller e model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
+
+Route::get('/', [PageController::class, 'homepage']);
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
