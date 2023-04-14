5a7b636a6fa553676824107c127c5fa5b7c11100
albertopiffer <albertopiffer96@gmail.com>
2023-04-14 16:53:15 +0200

    FEAT esercizio completo

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function homepage()
     {
-        return view('homepage');
+        $movies = Movie::all();
+        //dd($movies);
+
+        return view('welcome', ['movies' => $movies]);
     }
 }
